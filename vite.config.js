7eeec3552a682c36d77e2a8549c76f6e5b0f61a1
gowtham <gowthamdev078@gmail.com>
2025-11-17 03:16:01 +0530

    conflit fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-=======
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/honeybee-cafe/',   // <--- add this line
-  plugins: [react()]
->>>>>>> b2b44b3 (Initial commit)
+  base: '/honeybee-cafe/', // important for GitHub Pages
+  plugins: [react()],
 })
