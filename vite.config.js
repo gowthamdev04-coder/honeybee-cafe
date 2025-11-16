849ccdce7ac4b5030f0f6c6c07f5ad7bced68965
gowtham <gowthamdev078@gmail.com>
2025-11-17 01:30:54 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+<<<<<<< HEAD
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+=======
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/honeybee-cafe/',   // <--- add this line
+  plugins: [react()]
+>>>>>>> b2b44b3 (Initial commit)
 })
